02879b6e83b6f9f09ce72c91dbb0cba47456fc2e
celecoder <dagostinoceles@gmail.com>
2022-08-30 21:51:26 -0300

    modificaciones a adminsite

--- pagina_viajes/viajes/models.py
+++ pagina_viajes/viajes/models.py
@@ -17,6 +17,9 @@ class Paquete(models.Model):
     description = models.TextField()
     price= models.FloatField()
 
+    def __str__(self):
+        return self.name
+
 class Vuelo(models.Model):
     name = models.CharField(max_length=200)
     departure = models.TextField()
@@ -25,9 +28,19 @@ class Vuelo(models.Model):
     date_return = models.DateField(auto_now_add=True, null=True, blank=True)
     price= models.FloatField()
 
+    def __str__(self):
+        return self.name
+
 class Hotel(models.Model):
     name = models.CharField(max_length=200)
     location= models.TextField()
     date_departue = models.DateField(auto_now_add=True, null=True, blank=True)
     date_return = models.DateField(auto_now_add=True, null=True, blank=True)
     price= models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Hotel"
+        verbose_name_plural = "Hoteles"
