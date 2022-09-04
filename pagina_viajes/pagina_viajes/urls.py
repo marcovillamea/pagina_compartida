94467ec8508a292ab6bc86ff11696c33e94562e0
celecoder <dagostinoceles@gmail.com>
2022-09-04 19:30:52 -0300

    about agregado con boton

--- pagina_viajes/pagina_viajes/urls.py
+++ pagina_viajes/pagina_viajes/urls.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path,include
 from pagina_viajes.views import index, not_found
+from about.views import about_us
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +12,5 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path("", index, name="index"),
     path("not-found/",not_found, name ="notfound"),
+    path("about/", about_us, name ="about"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
